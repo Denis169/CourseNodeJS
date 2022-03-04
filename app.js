65299f85363b4ffe1655ce4908898ee3476e188c
Denis <denis6125038@gmail.com>
2022-03-04 12:22:57 +0300

    Use nodemon

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+const http = require('http');
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+let message = 'Hello Denis';
+
+http.createServer((request, response) => {
+
+  console.log(message);
+  response.end(message);
+
+}).listen(3000, '127.0.0.1', () => {
+  console.log('Сервер начал прослушивание запросов');
+})
